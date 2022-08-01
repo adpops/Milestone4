84055924064be864f78a8b2fb5832e05a804cf0b
Sumeet Sara <sumeetsara@d142-058-218-206.wireless.sfu.ca>
2022-08-01 11:17:33 -0700

    Made placeholder change

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #DB Initialization
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin'
+#app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'testing'
 mysql = MySQL(app)
 
